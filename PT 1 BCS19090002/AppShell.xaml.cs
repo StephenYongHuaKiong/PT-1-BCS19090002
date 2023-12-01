cf98b4b03c1adf34644289094c32a59c775ee2bc
StephenHuaKiong <150875546+StephenHuaKiong@users.noreply.github.com>
2023-12-01 15:40:46 +0800

    Add project files.

---
+++ PT 1 BCS19090002/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PT_1_BCS19090002;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PT 1 BCS19090002/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PT_1_BCS19090002;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
